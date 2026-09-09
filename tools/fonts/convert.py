638efe809ccae7b0893550c5a8a9ea6b04dd03d5
user <user@polarity.science>
2026-09-09 12:43:13 +0200

    feat: clean up code by removing unnecessary line breaks and improving formatting

--- tools/fonts/convert.py
+++ tools/fonts/convert.py
@@ -5,6 +5,11 @@
 # convert font formats based on file extension (i.e.: convert Gonville.otf Govillle.woff2)
 #
 
-from sys import argv; import fontforge; import psMat; import math
-f=fontforge.open(argv[1])
+import math
+from sys import argv
+
+import fontforge
+import psMat
+
+f = fontforge.open(argv[1])
 f.generate(argv[2])
